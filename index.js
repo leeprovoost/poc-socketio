16dac91648364a7b46ead987479904f80c5a2ae3
Lee Provoost <lee.provoost@gmail.com>
2014-12-03 15:24:13 +0000

    added ability to list users (/users)

--- index.js
+++ index.js
@@ -2,32 +2,43 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var userCount = 0;
+var clients = [];
+
 app.get('/', function(req, res){
   	res.sendfile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-	// assign automatic id to new client
-	io.emit('MSG', '>> New user ' + socket.id + ' connected.');
+	// new user has joined
+	userCount++;
+	clients.push(socket); 
+	socket.emit('receiveMessage', ">> Welcome, there are " + userCount + " users in the chat.")
+	socket.broadcast.emit('receiveMessage', '== New user ' + socket.id + ' connected.');
 
 	// clients disconnects
   	socket.on('disconnect', function(){
-		io.emit('MSG', '>> User ' + socket.id + ' disconnected.');
+  		userCount--;
+  		clients.splice(clients.indexOf(socket), 1);
+		io.emit('receiveMessage', '== User ' + socket.id + ' disconnected.');
   	});
 
   	// process commands and messages
-  	socket.on('MSG', function(msg){
+  	socket.on('sendMessage', function(msg){
   		if (msg.substr(0, 5) == "/nick") {
   			// TODO probably better to retain the socket.id and maintain a separate database for nicks
   			var old_nick = socket.id;
   			socket.id = msg.substr(6);
-  			socket.emit('MSG', ">> Hey " + socket.id + ", we've changed your nickname.");
-  			io.emit('MSG', old_nick + " has changed nickname to " + socket.id);
+  			socket.emit('receiveMessage', ">> Hey " + socket.id + ", we've changed your nickname.");
+  			socket.broadcast.emit('receiveMessage', "== " + old_nick + " has changed nickname to " + socket.id);
   		} else if (msg.substr(0, 6) == "/users") {
-
+  			socket.emit('receiveMessage', ">> List of users connected to this channel");
+	        clients.forEach(function(client, index) {
+	        	socket.emit('receiveMessage', ">> " + (index+1) + ") " + client.id);
+	        });
   		} else {
   			// emit message to all connected browsers
-    		io.emit('MSG', socket.id + ': ' + msg);
+    		socket.broadcast.emit('receiveMessage', socket.id + ': ' + msg);
     	}
   	});
 });
